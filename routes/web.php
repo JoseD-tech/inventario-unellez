7c28203f2cde6979e842c4dbe42df14f27047950
jgmting <jgmting@gmail.com>
2023-08-14 09:34:41 -0400

    final creo

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('welcome');
 
 Route::post('/formulario', [Controller::class, 'formulario'])->name('formulario');
 
